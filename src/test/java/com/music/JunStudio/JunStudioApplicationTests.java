be46f7c8b8e6098b81be25612febe5304f73994b
Jun Seo <112793862+junseo85@users.noreply.github.com>
2026-04-11 22:27:31 -0500

    setting up a CI/CD(Continuous Integration/Continuous Deployment)

--- src/test/java/com/music/JunStudio/JunStudioApplicationTests.java
+++ src/test/java/com/music/JunStudio/JunStudioApplicationTests.java
@@ -2,12 +2,15 @@ package com.music.JunStudio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // THIS IS THE MAGIC LINE
 class JunStudioApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// If this passes, it means Spring successfully built your app
+		// using the fake H2 database!
 	}
-
 }
